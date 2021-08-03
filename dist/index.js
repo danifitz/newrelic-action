27e6912aecba6cba991e10b12820dc3f97bdda76
Daniel Fitzgerald <danielfitzgerald@newrelic.com>
2021-08-03 11:23:48 +0100

    fixed function ordering in index.js

--- dist/index.js
+++ dist/index.js
@@ -6283,6 +6283,40 @@ const core = __nccwpck_require__(107);
 const github = __nccwpck_require__(28);
 const https = __nccwpck_require__(211);
 
+const sendToNewRelic = (region, accountId, insertKey, event) => {
+  const data = new TextEncoder().encode(JSON.stringify(event));
+  
+  // set the correct URL for Event API depending on region
+  const nrUrl = region == 'EU' ? 'insights-collector.eu01.nr-data.net': 'insights-collector.newrelic.com';
+
+  const options = {
+    hostname: nrUrl,
+    port: 443,
+    path: `/v1/accounts/${accountId}/events`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+      'X-Insert-Key': insertKey
+    }
+  }
+  
+  const req = https.request(options, res => {
+    console.log(`POST to New Relic statusCode: ${res.statusCode}`)
+  
+    res.on('data', d => {
+      process.stdout.write(d)
+    })
+  })
+  
+  req.on('error', error => {
+    console.error(error)
+  })
+  
+  req.write(data)
+  req.end()
+}
+
 try {
   // get the New Relic region and account ID
   const region = core.getInput('region');
@@ -6328,40 +6362,6 @@ try {
 
 } catch (error) {
   core.setFailed(error.message);
-}
-
-const sendToNewRelic = (region, accountId, insertKey, event) => {
-  const data = new TextEncoder().encode(JSON.stringify(event));
-  
-  // set the correct URL for Event API depending on region
-  const nrUrl = region == 'EU' ? 'insights-collector.eu01.nr-data.net': 'insights-collector.newrelic.com';
-
-  const options = {
-    hostname: nrUrl,
-    port: 443,
-    path: `/v1/accounts/${accountId}/events`,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length,
-      'X-Insert-Key': insertKey
-    }
-  }
-  
-  const req = https.request(options, res => {
-    console.log(`POST to New Relic statusCode: ${res.statusCode}`)
-  
-    res.on('data', d => {
-      process.stdout.write(d)
-    })
-  })
-  
-  req.on('error', error => {
-    console.error(error)
-  })
-  
-  req.write(data)
-  req.end()
 }
 
 })();
